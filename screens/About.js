410a8d93fa9e62de480c86d1fd97e26d94f31ee6
Shao Cherng Kong <55580912+jonksc@users.noreply.github.com>
2022-02-26 21:17:35 +0100

    Add description in about page

--- screens/About.js
+++ screens/About.js
@@ -5,7 +5,7 @@ import { globalStyles } from '../styles/global'
 const About = () => {
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.paragraphText}>Coffee Review is developed by Jon K.</Text>
+      <Text style={globalStyles.paragraphText}>Looking for the perfect coffee review app? Look no further than Coffee Review! This app offers coffee aficionados everything they need to evaluate and compare coffee brands, roast profiles, and brewing methods. Whether you’re a coffee enthusiast or just exploring specialty coffee, Coffee Review is the perfect app for you!</Text>
     </View>
   )
 }
